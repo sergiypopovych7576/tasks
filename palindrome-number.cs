37e989b871cbe502a35867818bfd7723b7b53d52
Popovych Serhii <popovychsergiy76@gmail.com>
2023-11-27 19:52:30 +0200

    Tasks

--- palindrome-number.cs
+++ palindrome-number.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LeetCodeTests
+﻿namespace LeetCodeTests
 {
-    internal class palindrome_number
+    public class Solution
     {
+        public bool IsPalindrome(int x)
+        {
+            var source = x.ToString();
+            var j = source.Length - 1;
+            for (var i = 0; i < source.Length; i++)
+            {
+                if (i == j)
+                {
+                    break;
+                }
+
+                if (source[i] != source[j])
+                {
+                    return false;
+                }
+                j--;
+            }
+            return true;
+        }
     }
 }
